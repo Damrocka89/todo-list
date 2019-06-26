392280189ef650dc712ef8aa61b6a22ddd5e2f98
Ewelina Łodyńska <ew.krasowska@hotmail.com>
2019-06-26 16:08:48 +0200

    Editing item added.

--- src/main/java/academy/learn/controller/TodoItemController.java
+++ src/main/java/academy/learn/controller/TodoItemController.java
@@ -41,8 +41,14 @@ public class TodoItemController {
 
     //http://localhost:8080/todolist/addItem
     @GetMapping(Mappings.ADD_ITEMS)
-    public String addEditItem(Model model) {
-        TodoItem todoItem = new TodoItem("", "", LocalDate.now());
+    public String addEditItem(@RequestParam(required = false, defaultValue = "-1") Integer id, Model model) {
+        log.info("editing id = {}", id);
+        TodoItem todoItem=todoItemService.getItem(id);
+
+        if (todoItem==null){
+            todoItem = new TodoItem("", "", LocalDate.now());
+        }
+
         model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
         return ViewNames.ADD_ITEM;
     }
@@ -50,7 +56,13 @@ public class TodoItemController {
     @PostMapping(Mappings.ADD_ITEMS)
     public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
         log.info("todoItem from form ={}", todoItem);
-        todoItemService.addItem(todoItem);
+
+        if (todoItem.getId()==null){
+            todoItemService.addItem(todoItem);
+        }else{
+            todoItemService.updateItem(todoItem);
+        }
+
         return "redirect:/" + Mappings.ITEMS;
     }
 
